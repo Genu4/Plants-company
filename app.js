43f651d617d0ca98b2920436d72a299fe2ee36f2
Gennadiy <glisovuy@ukr.net>
2022-12-09 19:03:13 +0200

    add mobile nav menu

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+//header menu
+
+
+
+
 // articles
 
 if (window.matchMedia("(max-width: 375px)").matches) {
@@ -54,29 +59,32 @@ if (window.matchMedia("(max-width: 375px)").matches) {
 
 let faqListContent = document.querySelectorAll(".faq-list-content");
 let faqImg = document.querySelectorAll(".faq-list-content-img");
-console.log(faqListContent);
-console.log(faqImg);
+let subFaqList = document.querySelectorAll(".sub-faq-list");
+
 
 for (let i = 0; i < faqListContent.length; i += 1) {
   faqListContent[i].addEventListener("click", function () {
-    let src = faqImg[i].getAttribute("src");
-    if (src === "./faq/images/plus.svg") {
-      faqImg[i].setAttribute("src", "./faq/images/minus.svg");
-    } else {
-      faqImg[i].setAttribute("src", "./faq/images/plus.svg");
-    }
-  });
-
-  faqListContent[i].addEventListener("click", function (e) {
-    if (e.target === faqListContent[i]) {
-      if (src === "./faq/images/plus.svg") {
-        faqImg[i].setAttribute("src", "./faq/images/minus.svg");
-      } else {
-        faqImg[i].setAttribute("src", "./faq/images/plus.svg");
-      }
-    }
+    subFaqList[i].classList.toggle("active");
+    faqListContent[i].classList.toggle("active");
+    faqImg[i].classList.toggle("active");
   });
 }
+
+$(document).on("mouseup", function (e) {
+ 
+  let flca = $(".faq-list-content.active"); 
+  let sfla = $(".sub-faq-list.active");
+  let flcia = $(".faq-list-content-img.active");
+  if (!flca.is(e.target) && flca.has(e.target).length === 0) {
+    
+    flca.removeClass("active"); 
+    sfla.removeClass("active");
+    flcia.removeClass("active");
+  }
+});
+
+
+/*mobile menu*/
 
 const navButtonMobile = document.querySelector(".nav-button-mobile");
 const navButtonIcon = document.querySelector(".nav-button-mobile-icon");
@@ -86,5 +94,22 @@ navButtonMobile.addEventListener("click", function () {
   navButtonIcon.classList.toggle("active");
   mobileNav.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
+});
 
+const mobileNavList = document.querySelector(".mobile-nav-list");
+
+mobileNavList.addEventListener("click", function (e) {
+  const mobileNavListItem = document.querySelectorAll(".mobile-nav-list-item");
+  const target = e.target;
+  Array.from(mobileNavListItem).forEach((item) => {
+    item.classList.remove("active");
+  });
+  target.classList.add("active");
+});
+
+const mobileNavListItem = document.querySelectorAll(".mobile-nav-list-item");
+const subMobileNavList = document.querySelector(".sub-mobile-nav-list");
+
+mobileNavListItem[1].addEventListener("click", function () {
+  subMobileNavList.classList.toggle("active");
 });
